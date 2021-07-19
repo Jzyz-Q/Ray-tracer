e8491d8b8d9fc5cd093f22cc4a7229ed98ff24c7
Jzyz-Q <82098682+Jzyz-Q@users.noreply.github.com>
2021-07-19 09:44:19 +0800

    Add files via upload

--- raytracer/src/camera.rs
+++ raytracer/src/camera.rs
@@ -16,16 +16,22 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new(lookfrom: Vec3, lookat: Vec3, vup: Vec3,
-               vfov: f64, aspect: f64, aperture: f64, focus_dist: f64)
-     -> Camera {
+    pub fn new(
+        lookfrom: Vec3,
+        lookat: Vec3,
+        vup: Vec3,
+        vfov: f64,
+        aspect: f64,
+        aperture: f64,
+        focus_dist: f64,
+    ) -> Camera {
         let theta = vfov * std::f64::consts::PI / 180.0;
-        let h_height: f64 = (theta/2.0).tan();
+        let h_height: f64 = (theta / 2.0).tan();
         let h_width: f64 = aspect * h_height;
 
         let w1 = (lookfrom - lookat).unit();
-        let u1 = (cross(vup, w1)).unit();
-        let v1 = cross(w1, u1);
+        let u1 = (Vec3::cross(vup, w1)).unit();
+        let v1 = Vec3::cross(w1, u1);
 
         Camera {
             sor: lookfrom,
@@ -33,22 +39,22 @@ impl Camera {
             u: u1,
             v: v1,
             cor: lookfrom
-                 - u1*h_width*focus_dist 
-                 - v1*h_height*focus_dist 
-                 - w1*focus_dist,
+                - u1 * h_width * focus_dist
+                - v1 * h_height * focus_dist
+                - w1 * focus_dist,
             hor: u1 * 2.0 * h_width * focus_dist,
             ver: v1 * 2.0 * h_height * focus_dist,
-            len_r: 0.5*aperture,
+            len_r: 0.5 * aperture,
         }
     }
 
     pub fn make_ray(&self, rng: &mut ThreadRng, a: f64, b: f64) -> Ray {
         let rd: Vec3 = random_in_unit_disk(rng) * self.len_r;
-        let offset: Vec3 = self.u*rd.x + self.v*rd.y;
+        let offset: Vec3 = self.u * rd.x + self.v * rd.y;
 
         let rt = Ray::new(
             self.sor + offset,
-            self.cor + self.hor*a + self.ver*b - self.sor - offset
+            self.cor + self.hor * a + self.ver * b - self.sor - offset,
         );
         return rt;
     }
