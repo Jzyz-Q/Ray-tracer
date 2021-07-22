bfbd73ddc58b50f80d06e2942f57200c85025db5
Jzyz-Q <82098682+Jzyz-Q@users.noreply.github.com>
2021-07-23 02:24:21 +0800

    Add files via upload

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -56,10 +56,10 @@ fn main() {
         is_ci, n_jobs, n_workers
     );
 
-    let width = 600;
-    let height = 600;
-    let spp = 8000;
-    let max_depth = 100;
+    let width = 700;
+    let height = 700;
+    let spp = 10000;
+    let max_depth = 500;
     let background = Vec3::zero();
 
     let (tx, rx) = channel();
@@ -533,7 +533,7 @@ fn final_scene() -> Hlist {
     let mut boxes1 = Hlist::new(true);
 
     // light
-    let vl = Arc::<Solid>::new(Solid::new(Vec3::new(20.0, 20.0, 20.0)));
+    let vl = Arc::<Solid>::new(Solid::new(Vec3::new(18.0, 18.0, 18.0)));
     let light = Arc::<Diffuse>::new(Diffuse::new(vl));
     objects.push(Arc::<Xzrect>::new(Xzrect::new(
         203.0, 343.0, 227.0, 332.0, 554.0, light,
